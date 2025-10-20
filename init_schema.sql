4da762bc5c8acf09a0eb64c44a9673cf1b1141a6
Ridvik Pal <ridvikpal@gmail.com>
2025-10-20 18:40:56 -0400

    Fixed a bunch of syntax errors in the init_schema.sql file.

--- init_schema.sql
+++ init_schema.sql
@@ -1,23 +1,25 @@
-CREATE TABLE if NOT EXISTS user {
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
     user_uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     first_name TEXT,
     last_name TEXT
-}
+);
 
-INSERT INTO user (first_name, last_name) VALUES
-    ("Niraaj", "Rajalingam"),
-    ("Ridvik", "Pal"),
-    ("Eduardo Jose", "Sampaio Martins"),
-    ("Matthew", "Grech"),
-    ("Areeba", "Mobeen");
+INSERT INTO users (first_name, last_name) VALUES
+    ('Niraaj', 'Rajalingam'),
+    ('Ridvik', 'Pal'),
+    ('Eduardo Jose', 'Sampaio Martins'),
+    ('Matthew', 'Grech'),
+    ('Areeba', 'Mobeen');
 
-CREATE TABLE IF NOT EXISTS tamagotchi (
+CREATE TABLE IF NOT EXISTS tamagotchis (
     tamagotchi_uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_uuid UUID REFERENCES user (user_uuid),
+    user_uuid UUID REFERENCES users (user_uuid),
     image_path TEXT
 );
 
-INSERT INTO tamagotchi (image_path) VALUES
+INSERT INTO tamagotchis (image_path) VALUES
     ('test 1'),
     ('test 2'),
     ('test 3');

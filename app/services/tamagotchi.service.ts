5a5bfe09b69e2057eb56a385919547a9e1eabb48
Ridvik Pal <ridvikpal@gmail.com>
2025-10-22 15:04:46 -0400

    Added logic for getTamagotchisForUser.

--- app/services/tamagotchi.service.ts
+++ app/services/tamagotchi.service.ts
@@ -1,11 +1,31 @@
-interface ICreateTamagotchiParams {
-  user_uuid: string;
-  image_path: string;
+import { Tamagotchi } from "../types/tamagotchi.type";
+import { poolQuery } from "./database.service";
+
+export const getTamagotchisForUser = async (
+  user_uuid: string
+): Promise<Tamagotchi[] | undefined> => {
+  const result: Tamagotchi[] | undefined = await poolQuery(
+    `SELECT * FROM tamagotchis
+    WHERE user_uuid = '${user_uuid}';`
+  );
+
+  if (!result) {
+    console.log("An error occurred retrieving the tamagotchi");
+    return;
+  }
+  
+  return result;
 }
 
-export const createTamagotchi = async ({
-  user_uuid,
-  image_path
-}: ICreateTamagotchiParams) => {
-    
+export const createTamagotchiForUser = async (
+  user_uuid: string,
+  image_path: string
+) => {
+}
+
+export const editTamagotchiForUser = async (
+  user_uuid: string,
+  image_path: string
+) => {
+
 }

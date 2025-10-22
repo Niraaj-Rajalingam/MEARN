664763eb15c6ec83d7221e2dcf164b624d5563b5
Ridvik Pal <ridvikpal@gmail.com>
2025-10-22 13:56:18 -0400

    Updated database service to use singleton pool with safe poolClientQuery() function for optimal performance.

--- app/services/database.service.ts
+++ app/services/database.service.ts
@@ -1,20 +1,35 @@
 import { Pool } from 'pg';
 
-export const createPool = (): Pool => {
-  const pool = new Pool({
-    user: 'postgres',
-    password: 'mearn-app-db',
-    host: 'localhost',
-    port: 5432,
-    database: 'postgres',
-    max: 20, // max number of connections
-    idleTimeoutMillis: 30000, // max idle time
-    connectionTimeoutMillis: 2000, // connection timeout
-  });
+export const pool = new Pool({
+  user: 'postgres',
+  password: 'mearn-app-db',
+  host: 'localhost',
+  port: 5432,
+  database: 'postgres',
+  max: 20, // max number of connections
+  idleTimeoutMillis: 30000, // max idle time
+  connectionTimeoutMillis: 2000, // connection timeout
+});
 
-  return pool;
+interface IPoolClientQueryProps {
+  queryString: string;
+  queryStringParams: (string | number | boolean)[]
 }
 
-export const destroyPool = (pool: Pool) => {
-  pool.end();
+export const poolClientQuery = async ({
+  queryString,
+  queryStringParams
+}: IPoolClientQueryProps) => {
+  const client = await pool.connect();
+  try {
+    const result = await client.query(queryString, queryStringParams);
+    return result.rows;
+  } catch (error) {
+    console.log(`An error occurred when executing db query: 
+      ${queryString}
+      with params:
+      ${queryStringParams}`);
+  } finally {
+    client.release();
+  }
 }

91005505ccb5410acb2eb45097dcb9d6cabff130
Niraaj Rajalingam <rajalingam.niraaj@gmail.com>
2025-10-29 14:20:55 -0400

    task service update

--- app/types/task.type.ts
+++ app/types/task.type.ts
@@ -2,22 +2,29 @@ import { UUID } from "crypto";
 
 export type Task = {
   todo_uuid: UUID;
-  user_uuid: UUID;
+  parent_task_uuid: UUID | null;
+  created_by: UUID;
+  group_uuid: UUID | null;
   title: string;
   description?: string;
   created_at: Date;
   due_date?: Date;
   completed_at?: Date;
-  priority: 1 | 2 | 3;  // 1: Low, 2: Medium, 3: High
-  status: 'pending' | 'in_progress' | 'completed' | 'cancelled';
+  priority: 1 | 2 | 3;
+  status: 'draft' | 'pending' | 'in_progress' | 'completed' | 'cancelled';
+  assignees: UUID[];
 }
 
 export type CreateTaskDTO = {
-  user_uuid: UUID;
+  created_by: UUID;
+  assignee_uuids: UUID[];
   title: string;
+  parent_task_uuid?: UUID;
+  group_uuid?: UUID;
   description?: string;
   due_date?: Date;
   priority?: 1 | 2 | 3;
+  status?: 'draft' | 'pending' | 'in_progress' | 'completed' | 'cancelled';
 }
 
 export type UpdateTaskDTO = {
@@ -25,6 +32,9 @@ export type UpdateTaskDTO = {
   description?: string;
   due_date?: Date;
   priority?: 1 | 2 | 3;
-  status?: 'pending' | 'in_progress' | 'completed' | 'cancelled';
+  status?: 'draft' | 'pending' | 'in_progress' | 'completed' | 'cancelled';
   completed_at?: Date;
+  parent_task_uuid?: UUID | null;
+  group_uuid?: UUID | null;
+  assignee_uuids?: UUID[];
 }
